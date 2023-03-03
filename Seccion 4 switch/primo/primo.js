343fea1f4584ad4672e4a798f4d538aea9e5cfd0
Sergio Chica <sergioanjaci@gmail.com>
2023-03-03 15:22:52 -0500

    correccion

--- Seccion 4 switch/primo/primo.js
+++ Seccion 4 switch/primo/primo.js
@@ -1,4 +1,4 @@
-let numero = parseInt(prompt("Ingrese un numero"))
+let numero = parseInt(prompt("Ingrese un numero de 1 a 15"))
 let primo = numero == 2 || numero == 3 || numero == 5 || numero == 7 
 || numero == 11 || numero == 13;
 
