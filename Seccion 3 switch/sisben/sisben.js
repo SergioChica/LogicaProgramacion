9ea42b98599edff78a4cb994184f41da25a6abbb
Sergio Chica <sergioanjaci@gmail.com>
2023-03-03 15:12:09 -0500

    Taller switch

---
+++ Seccion 3 switch/sisben/sisben.js
@@ -0,0 +1,19 @@
+//Crea un programa que recciba el sisben de un usuario y muestre si este tiene derecho a un subsidio o no.
+//Si el sisben es 1, 2 o 3, el usuario tiene derecho
+
+let sisben = parseInt(prompt("Ingrese sisben"));
+switch (sisben) {
+    case 1:
+        console.log("Tiene derecho a subsidio");
+        break;
+    case 2:
+        console.log("Tiene derecho a subsidio");
+        break;
+    case 3:
+        console.log("Tiene derecho a subsidio");
+        break;
+
+    default:
+        console.log("No tiene derecho a subsidio");
+        break;
+}
---
+++ Seccion 4 switch/primo/primo.js
@@ -0,0 +1,13 @@
+let numero = parseInt(prompt("Ingrese un numero"))
+let primo = numero == 2 || numero == 3 || numero == 5 || numero == 7 
+|| numero == 11 || numero == 13;
+
+switch (primo) {
+    case true:
+        console.log("El numero es primo");
+        break;
+
+    default:
+        console.log("El numero no es primo");
+        break;
+}
