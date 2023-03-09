f266fc44b485d00ad6c9a9a51afb6b883d26d3e5
Sergio Chica <sergioanjaci@gmail.com>
2023-03-09 17:00:17 -0500

    Condicionales Switch

---
+++ ejemplo/includes.js
@@ -0,0 +1,6 @@
+let contieneGuion = "jasjasdasdasd-asdadasd".includes("-");
+console.log("contiene guion:", contieneGuion);
+let saludoMaria = "hola Maria 2023".includes("Maria 2024");
+console.log("Incluye maria:", saludoMaria);
+let saludo = "hola adso tarde 2023";
+console.log("la variable saludo tiene", saludo.length);
